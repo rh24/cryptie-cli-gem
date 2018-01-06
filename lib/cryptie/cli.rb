7b210c0276f309039757b0480dff74d56daad78f
rh24 <ms.rebeccahong@gmail.com>
2018-01-06 18:00:17 -0500

    Add Cryptie::Coin self.learn method
    This method will take user input and puts attributes from a coin specific coin instance.

--- lib/cryptie/cli.rb
+++ lib/cryptie/cli.rb
@@ -5,38 +5,29 @@ class Cryptie::CLI
   def call
     Cryptie::Scraper.create_coins
     Cryptie::Coin.list
-    menu
-    goodbye
+    input = gets.strip.to_i
+    Cryptie::Coin.learn(input)
+    # menu
+    # goodbye
   end
 
   def menu
     puts "Which coin would you like to learn more about? Enter rank \#:"
-    # input = gets.strip.to_i # Sanitize the input later
     input = nil
     while input != "exit" do
       input = gets.strip
       case input
-      when input.to_i.is_a?(Integer) # this line is broken
-        learn(input) # entered 1, but nothing is printing. terminal is stuck
-      when "exit"
-        puts "See you later!"
+      when input.is_a?(Integer) # this line is broken
+        Cryptie::Coin.learn(input) # entered 1, but nothing is printing. terminal is stuck until i exit
       when input == "list" # this line is broken
         Cryptie::Coin.list
+      when "exit"
+        puts "See you later!"
       # else
       #   puts "Not sure what you want. Try again."
       #   puts "Please, enter a number 1-30, or \"exit\" to leave:"
       end
     end
-  end
-
-  def learn(input)
-    coin = Cryptie::Coin.all[input]
-    puts "You've entered #{input}."
-    puts "Name: #{coin.name}"
-    puts "Symbol: #{coin.symbol}"
-    puts "Price: #{coin.price}"
-    puts "Supply: #{coin.supply}"
-    puts "Market Cap: #{coin.market_cap}"
   end
 
   def goodbye
--- lib/cryptie/coin.rb
+++ lib/cryptie/coin.rb
@@ -7,36 +7,42 @@ class Cryptie::Coin
 
   def initialize(coin_hash)
     coin_hash.each {|key, value| self.send("#{key}=", value)}
-    # binding.pry
     self.class.all << self
+  end
+
+  def self.learn(input)
+    coin = self.all[input-1]
+    puts "\nYou've entered ##{input}:"
+    puts "\nName: #{coin.name}"
+    puts "Symbol: #{coin.symbol}"
+    puts "Price: #{coin.price}"
+    puts "Supply: #{coin.supply}"
+    puts "Market Cap: #{coin.market_cap}"
+    if coin.supply.include?("*")
+      coin.supply += " this coin is not mineable"
+    else
+      nil
+    end
+    # binding.pry
   end
 
   def self.list
     @list =
     puts "Today's Top 10 Cryptocurrencies by Market Cap:"
     Cryptie::Scraper.all.each.with_index(1) do |coin_hash, index|
-      # binding.pry
       puts "#{index}. #{coin_hash[:name]}  #{coin_hash[:symbol]}   \n"
       break if index == 10
     end
   end
 
-  def self.full_list
+  def self.full_list # Not sure if I want to include this method at all. CLI gets messy
     @full_list =
     puts "Today's Top 10 Cryptocurrencies by Market Cap:"
     # puts "#{table_head_array.join("  ")}\n\n" if I want to display all attributes in one full list
-    sym_array.each.with_index(1) do |sym, index|
-      puts "#{index}. #{name_array[index-1]}  #{sym}   \n"
+    Cryptie::Scraper.all.each.with_index(1) do |coin_hash, index|
+      puts "#{index}. #{coin_hash[:name]}  #{coin_hash[:symbol]}   \n"
       break if index == 10
     end
-  end
-
-  def learn(input)
-    input = input.to_i
-    index = input-1
-    puts "#{input}. #{self.all[index].name} #{self.all[index].symbol}\n"
-    puts "Price: #{self.all[index].price}\n Market Cap: #{self.all[index].market_cap}\n Circulating Supply: #{self.all[index].supply}\n 24h Trading Vol: #{self.all[index].volume}\n"
-    puts "Note: * not mineable"
   end
 
   def self.find_by_rank(input)
