c07452d56a3ac0720bbf05444a6274e21b7aad84
rh24 <ms.rebeccahong@gmail.com>
2018-01-08 15:57:03 -0500

    Add compressed screen capture video

--- lib/cryptie/order.rb
+++ lib/cryptie/order.rb
@@ -1,8 +1,8 @@
 class Cryptie::Order
   attr_accessor :person
 
-  def intialize(coin, quantity)
-    
+  def intialize(coin, quantity) # Check if the person has enough spending_balance before order is instantiated
+
     self.person.orders << self
   end
 
