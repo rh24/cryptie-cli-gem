fca1b3b933b7746efbf267fada213541fc9ba38a
rh24 <ms.rebeccahong@gmail.com>
2018-01-08 12:56:24 -0500

    Remove stale 'cryptie_spec' comments

--- spec/cryptie_spec.rb
+++ spec/cryptie_spec.rb
@@ -5,10 +5,6 @@ describe "Cryptie::Coin" do
   # {:rank => "2", :name => "Ripple", :symbol => "XRP"}
   # {:rank => "3", :name => "Ethereum", :symbol => "ETH"}
 
-  # after(:each) do
-  #   Cryptie::Coin.class_variable_set(:@@all, [])
-  # end
-
   describe "#new" do
     it "takes in an argument of a hash and sets each new coin's attributes using the key value pairs of its hash." do
       expect{Cryptie::Coin.new(coin_hash)}.to_not raise_error
