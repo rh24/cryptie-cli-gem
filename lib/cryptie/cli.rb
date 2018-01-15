4b140c1854da55dacba91e1a56e313c781549a7a
rh24 <ms.rebeccahong@gmail.com>
2018-01-15 18:07:53 -0500

    Replace CLI puts statements with single quotes in place of double quotes

--- lib/cryptie/cli.rb
+++ lib/cryptie/cli.rb
@@ -19,14 +19,14 @@ class Cryptie::CLI
 
   def greeting
     puts "\n--------------------- Which coin would you like to learn more about? ------------------------"
-    puts "-------------------- Enter rank #, \"list\" to see top coins, or \"exit\" -----------------------"
-    puts "------ NOTE: If you'd prefer to search all coins by your token's symbol, type \"symbol\" ------"
-    puts "--------- If you'd like to go straight to ordering, enter \"order\" to get started. ------------"
+    puts "-------------------- Enter rank #, 'list' to see top coins, or 'exit' -----------------------"
+    puts "------ NOTE: If you'd prefer to search all coins by your token's symbol, type 'symbol' ------"
+    puts "--------- If you'd like to go straight to ordering, enter 'order' to get started. -----------"
   end
 
   def return_menu
-    puts "Welcome back! Enter \"list\",   rank #,   \"symbol\",   \"order\" (for new accounts),
-    \"buy more\" (to continue purchasing),  or \"exit\" to quit."
+    puts "Welcome back! Enter 'list',   rank #,   'symbol',   'order' (for new accounts),
+    'buy more' (to continue purchasing),  or 'exit' to quit."
     menu
   end
 
@@ -37,13 +37,13 @@ class Cryptie::CLI
       if input.to_i <= Cryptie::Coin.all.size && input.to_i > 0
         coin = Cryptie::Coin.find_by_rank(input)
         print_info(coin)
-        puts "\nWould you like to place an order? Enter \"order\" to get started or \"menu\" for more options."
+        puts "\nWould you like to place an order? Enter 'order' to get started or 'menu' for more options."
       elsif input == "symbol"
-        puts "---------------- SEARCH BY SYMBOL: ---------------- \nWhen finished, enter \"menu\" for more options."
+        puts "---------------- SEARCH BY SYMBOL: ---------------- \nWhen finished, enter 'menu' for more options."
         symbol until input == "menu"
       elsif input == "list"
         Cryptie::Coin.list
-        puts "\nWould you like to place an order? Enter \"order\" to get started or \"menu\" for more options."
+        puts "\nWould you like to place an order? Enter 'order' to get started or 'menu' for more options."
       elsif input == "order"
         order
       elsif input == "buy more"
@@ -53,7 +53,7 @@ class Cryptie::CLI
       elsif input == "menu" # If menu is explicitly called again, then print a shortened list of options
         return_menu
       else
-        puts "Not sure what you want. Try again, or enter \"exit\" to leave:"
+        puts "Not sure what you want. Try again, or enter 'exit' to leave:"
       end
     end
   end
@@ -63,7 +63,7 @@ class Cryptie::CLI
     @person = Cryptie::Person.new(person_name, balance)
     person.order
     person.display_account
-    puts "--- Enter \"buy more\" to make another purchase, \"menu\" for more options, or \"exit\" to quit.---"
+    puts "--- Enter 'buy more' to make another purchase, 'menu' for more options, or 'exit' to quit.---"
     puts "------- NOTE: If you already know your token symbol, simply type it into your order. --------"
     puts "------- The CLI only displays the Top 20 coins, but all token info has been scraped. --------"
     puts "------------------------- Check to see if we have your coin! --------------------------------"
@@ -76,7 +76,7 @@ class Cryptie::CLI
     if input.downcase == "menu"
       return_menu
     elsif coin == nil
-      puts "Sorry, we couldn't find your token. Enter valid symbol or \"menu\" for more options"
+      puts "Sorry, we couldn't find your token. Enter valid symbol or 'menu' for more options"
       symbol
     else
       print_info(coin)
@@ -85,7 +85,7 @@ class Cryptie::CLI
 
   def buy_more
     if person == nil
-      puts "Create an account first. Enter \"order\" to get started."
+      puts "Create an account first. Enter 'order' to get started."
       menu
     elsif person.spending_balance <= 0
       puts "You're out of money! Would you like to continue purchasing? (y/n)"
@@ -94,8 +94,8 @@ class Cryptie::CLI
         person.spending_balance = balance
         person.order
         person.display_account
-        puts "-- Enter \"buy more\" to make another purchase, \"menu\" for more options, or \"exit\" to quit. --"
-        puts "----------- Once you enter \"list\" or \"exit\" your information will not be saved -------------"
+        puts "-- Enter 'buy more' to make another purchase, 'menu' for more options, or 'exit' to quit. --"
+        puts "----------- Once you enter 'list' or 'exit' your information will not be saved -------------"
       elsif answer == "no" || answer == "n"
         return_menu
       else
@@ -105,7 +105,7 @@ class Cryptie::CLI
     else
       person.order
       person.display_account
-      puts "-- Enter \"buy more\" to make another purchase, \"menu\" for more options, or \"exit\" to quit. --"
+      puts "-- Enter 'buy more' to make another purchase, 'menu' for more options, or 'exit' to quit. --"
     end
   end
 
@@ -130,7 +130,7 @@ class Cryptie::CLI
     elsif input == "menu"
       return_menu
     elsif input.to_i.is_a?(Integer) && input.to_i != 0
-      puts "Need valid name. Try again, \"exit\" or \"menu\" for more options."
+      puts "Need valid name. Try again, 'exit' or 'menu' for more options."
       person_name
     else
       input
@@ -144,7 +144,7 @@ class Cryptie::CLI
     if input.downcase == "menu"
       return_menu
     elsif input.to_i <= 0
-      puts "You have no money to spend. Please, enter valid balance, \"exit\", or type \"menu\" for more options."
+      puts "You have no money to spend. Please, enter valid balance, 'exit', or type 'menu' for more options."
       balance
     else
       input.to_i
